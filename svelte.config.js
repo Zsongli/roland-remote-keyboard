e85ae1617b4a72a4793d189dfa3cfe1c7893ad61
Zsong Lee <zsongli@gmail.com>
2021-10-20 22:41:48 +0200

    static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
-
 	kit: {
-		target: '#svelte'
+		adapter: adapter({
+			assets: "build",
+			pages: "build",
+			fallback: null
+		}),
+		target: "#svelte"
 	}
 };
 
